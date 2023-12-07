81d651cbafb491bfdeb3b92fb1cc1de7f962d74b
ValeraPrus <146940361+ValeraPrus@users.noreply.github.com>
2023-12-07 16:04:14 +0200

    Update HW_m9.py

--- HW_m9.py
+++ HW_m9.py
@@ -1,30 +1,30 @@
 exit_list = ['good bye', 'close', 'exit']
 
 command_list = 'Command list:\n-hello\n-add ...\
-          \n-change ...\n-phone ....\n-show all\
+          \n-change ...\n-phone ....\n-show_all\
           \n-good bye\n-close\n-exit'
 
 phone_dict = {}
 
 
 def main():
+    handlers = {
+        'hello': hello_func,
+        'add': add_func,
+        'change': change_func,
+        'phone': phone_func,
+        'show_all': show_all_func,
+               }
+
     while True:
         user = input('>>> ').lower()
         user_date = user.split(' ')
-        user_date.pop(0)
+        command_handler = user_date.pop(0)
         if user in exit_list:
             print(exit_func())
             break
-        elif 'hello' in user:
-            print(hello_func())
-        elif 'add ' in user:
-            print(add_func(user_date))
-        elif 'change ' in user:
-            print(change_func(user_date))
-        elif 'phone ' in user:
-            print(phone_func(user_date))
-        elif 'show all' in user:
-            print(show_all_func())
+        elif command_handler in handlers:
+            print(handlers[command_handler](user_date))
         else:
             print('- Wrong command')
 
@@ -44,20 +44,15 @@ def input_error(func):
 
 @input_error
 def add_func(user_date):
-    if not user_date[0] in phone_dict:
-        phone_dict[user_date[0]] = user_date[1]
-        return f'- New contact "{user_date[0]}" added'
-    else:
-        return f'- Name "{user_date[0]}" has already created'
+    phone_dict.setdefault(user_date[0], user_date[1])
+    return f'- New contact "{user_date[0]}" added'
 
 
 @input_error
 def change_func(user_date):
-    if user_date[0] in phone_dict:
-        phone_dict[user_date[0]] = user_date[1]
-        return f'- Number of "{user_date[0]}" changed'
-    else:
-        return f'- Name "{user_date[0]}" not found'
+    del phone_dict[user_date[0]]
+    phone_dict[user_date[0]] = user_date[1]
+    return f'- Number of "{user_date[0]}" changed'
 
 
 @input_error
@@ -69,16 +64,17 @@ def exit_func():
     return '- Good bye!'
 
 
-def hello_func():
+def hello_func(arg):
     return '- How can I help you?'
 
 
-def show_all_func():
+def show_all_func(arg):
     return phone_dict
 
 
 if __name__ == '__main__':
     print(f'- Hello :)\n\n{command_list}')
     main()
+
 
 
